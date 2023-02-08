0ce6abdbbe81849de116dee32c778b08a1c54efa
vasilyl1 <likhovaido@gmail.com>
2023-02-07 19:32:00 -0500

    fs and inq definitions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,43 @@
-// TODO: Include packages needed for this application
+const fs = require('fs'); // filesystem init
+const inq = require('inquirer'); // inquirer init
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    inq
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your user name?',
+                name: 'username',
+            },
+            {
+                type: 'password',
+                message: 'What is your password?',
+                name: 'password',
+            },
+            {
+                type: 'password',
+                message: 'Re-enter password to confirm:',
+                name: 'confirm',
+            },
+        ])
+        .then((response) =>
+            response.confirm === response.password
+                ? console.log('Success!')
+                : console.log('You forgot your password already?!')
+        );
+
+    fs.readFile('data.csv', 'utf8', (error, data) =>
+        error ? console.error(error) : console.log(data)
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
